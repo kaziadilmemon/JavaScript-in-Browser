fec2997bb064e133060686d6d50bd7f02bc32dd3
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-17 00:27:09 +0500

    Create JavaScript Console Object.js

---
+++ JavaScript Console Object.js
@@ -0,0 +1,25 @@
+// The Console Object has several methods, log being one of them.Some of them are as follows:
+
+// 1. log() : Outputs a message to the console.
+console.log("Hi Adil")
+
+// 2. assert() : Used to assert a condition.
+console.assert(5>26) // this will gives a message of Assertion failed
+console.assert(5<26) /* this will shows no any error there because the 
+                        condtion is true.*/
+
+// 3. clear() : clears the console
+console.clear()
+
+// 4. table() : Displays a tabular data
+obj={a:1, b:2, c:3}
+console.table(obj)
+
+// 5. warn() : Used for Warnings
+console.warn("hey! please check it out")
+
+// 6. error() : used for errors
+console.error("This is an Error")
+
+//7. Info() : used for special information
+console.info("This is related information")
