a15786da5deaef6599879ab6b4d732a34102ad36
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-17 01:16:48 +0500

    Update JavaScript alert,prompt and confirm.js

--- JavaScript alert,prompt and confirm.js
+++ JavaScript alert,prompt and confirm.js
@@ -2,12 +2,14 @@
 alert("Hi, How are you!")
 
 // prompt: used to take user input as string.
+
 alert("Enter the value of a!")
 let a = prompt("Enter a here", "57")
 a = Number.parseInt(a)
 alert("You entered a of type " + (typeof a))
 
 // confirm: shows a message and wants for the user to press ok or cancel returns true for ok and false for cancel.
+
 let write = confirm("Do you want to write it to the page there?")
 if (write) {
   document.write(a)
