579baefa96fdabadf3f05fa44304183af65c8b62
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-20 01:06:12 +0500

    Create Table Navigation.js

---
+++ Table Navigation.js
@@ -0,0 +1,8 @@
+let d = document.body.firstElementChild.firstElementChild
+console.log(d)
+console.log(d.rows)
+console.log(d.caption)
+console.log(d.tHead.firstElementChild)
+console.log(d.tFoot)
+console.log(d.tBodies)
+console.log(d.rows[1].rowIndex)
