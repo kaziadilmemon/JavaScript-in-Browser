56a3f06930ac48e52a1d25ba06c91a92e2ab59f3
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-19 23:28:22 +0500

    Create Accessing Children of an Element.js

---
+++ Accessing Children of an Element.js
@@ -0,0 +1,11 @@
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+let arr = Array.from(document.body.childNodes)
+console.log(arr)
+
+/* Direct as well as deeply nested elements of an element are called its children.
+child nodes => Element that are direct children 
+For example: head and body are children of <html>
+
+  ChildNodes looks like an array but it not actually an array but a collective we can use Arrayfrom(collection) to convert it into an 
+  Array. (Array methods can't work)
