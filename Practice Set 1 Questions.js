6dc8c607b0276bf11b876bbcca609afb8aa911ee
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-19 20:13:59 +0500

    Update Practice Set 1 Questions.js

--- Practice Set 1 Questions.js
+++ Practice Set 1 Questions.js
@@ -58,3 +58,12 @@ while (runAgain) {
   }
   runAgain = confirm("Do you want to play again?")
 }
+
+/* Question 4 : Write a program to change the url to google.com (Redirection) if user enters a number greater than 4. */
+
+let number = prompt("Enter your number")
+number = Number.parseInt(number)
+
+if (number > 4) {
+  location.href = "https://google.com"
+}
