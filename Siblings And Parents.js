17d445a9c75bf830598d2d77156a145933d24189
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-19 23:59:02 +0500

    Update Siblings And Parents.js

--- Siblings And Parents.js
+++ Siblings And Parents.js
@@ -8,3 +8,15 @@ console.log(a.firstChild.nextSibling)
 => They are read only.
 => They are live elem.Child Nodes variables (reference) will automatically update if child Nodes of elem is changed.
 => They are iterable using for.....of Loop.
+
+Siblings and the Parent:
+=>  Siblings are nodes that are children of the same parent.
+
+For Example: <head> and <body> are Siblings. 
+  Siblings have same parent.In the above example its HTML.
+
+=> <body> is said to be the "next" or "right" sibling of <head>,<head> is said to be the "previous" are "left" sibling of <body>
+
+=> The next sibling is in next Sibling property, and the previous one in previous String
+
+  
