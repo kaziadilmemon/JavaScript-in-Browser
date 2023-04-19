b4e86fe44914c7efb5655ff3c762b8b4acfc6ac2
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-19 23:50:27 +0500

    Update Siblings And Parents.js

--- Siblings And Parents.js
+++ Siblings And Parents.js
@@ -3,3 +3,8 @@ a = document.body.firstChild
 console.log(a.parentNode)
 console.log(a.parentElement)
 console.log(a.firstChild.nextSibling)
+
+/* DOM Collections:
+=> They are read only.
+=> They are live elem.Child Nodes variables (reference) will automatically update if child Nodes of elem is changed.
+=> They are iterable using for.....of Loop.
