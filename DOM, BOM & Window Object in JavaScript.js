6894c00fd01cd47bcf0ea071b7f604b56e97e124
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-19 16:30:08 +0500

    Update DOM, BOM & Window Object in JavaScript.js

--- DOM, BOM & Window Object in JavaScript.js
+++ DOM, BOM & Window Object in JavaScript.js
@@ -5,7 +5,8 @@ console.log(window)
 console.log(document.body) // Page Body as JS object.
 document.body.style.background="Yellow" // Changes page background to Yellow
 
-/* Browser Object Model (BOM) : The Browser object model represents additional objects provided by the browser (host environment) for working with everyting except the document. 
+/* Browser Object Model (BOM) : The Browser object model represents additional objects provided by the browser (host environment) for working 
+with everyting except the document. 
 The functions alert/confirm/prompt are also a part of BOM */
  // like we can use location href
 
