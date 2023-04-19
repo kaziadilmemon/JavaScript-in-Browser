fb0240852790a7153a7eeb9ced8e8087200443fe
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-19 16:29:28 +0500

    Create DOM, BOM & Window Object in JavaScript.js

---
+++ DOM, BOM & Window Object in JavaScript.js
@@ -0,0 +1,11 @@
+/* Window Object: window object represents browser window and provides methods to control it. It is a global Object.*/
+console.log(window)
+
+// Document Object Model (DOM) : DOM represents the page content as HTML.
+console.log(document.body) // Page Body as JS object.
+document.body.style.background="Yellow" // Changes page background to Yellow
+
+/* Browser Object Model (BOM) : The Browser object model represents additional objects provided by the browser (host environment) for working with everyting except the document. 
+The functions alert/confirm/prompt are also a part of BOM */
+ // like we can use location href
+
