c492cd85b1bb5da9c19a833ce6ae77655bab1bbf
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-20 20:42:11 +0500

    Update matches,closest & contains in JavaScript.js

--- matches,closest & contains in JavaScript.js
+++ matches,closest & contains in JavaScript.js
@@ -5,9 +5,12 @@ console.log(id1)
 
 console.log(id1.matches(".class"))
 console.log(id1.matches(".box"))
-
+/* elem.closest(CSS): To look for the nearest ancestor that matches the given CSS-selector.The elem itself is also checked. */
 
 console.log(sp1.closest("#sp1"))
+
+/* elemA.contains(elem) : Returned true if elemB is inside elemA (a descendant of elemA) or when elemA==elemB */
+
 console.log(id1.contains(sp1))
 console.log(sp1.contains(sp1))
 console.log(sp1.contains(id1))
