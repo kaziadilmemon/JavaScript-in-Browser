a4962b11b847e7c12976894a0d11720c36870a6c
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-20 22:48:32 +0500

    Delete Question no 2 on DOM manipulation directory

--- Question no 2 on DOM manipulation/script.js
+++
@@ -1,7 +0,0 @@
-/* Question : Create an element with 3 children.Now Change the color of first and last element to green.*/
-
-document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
-
-document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
-
-document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
