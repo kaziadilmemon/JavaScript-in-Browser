445ee09c9a113cd35a06c2c3c76743eaa01f751b
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-20 20:37:58 +0500

    Create matches,closest & contains in JavaScript.js

---
+++ matches,closest & contains in JavaScript.js
@@ -0,0 +1,13 @@
+let id1 = document.getElementById("id1")
+let sp1 = document.getElementById("sp1")
+console.log(id1)
+/* elem.matches(CSS): to check if element matches the given CSS Selector. */
+
+console.log(id1.matches(".class"))
+console.log(id1.matches(".box"))
+
+
+console.log(sp1.closest("#sp1"))
+console.log(id1.contains(sp1))
+console.log(sp1.contains(sp1))
+console.log(sp1.contains(id1))
