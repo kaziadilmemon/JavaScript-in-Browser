d4f51f2a8a3495c160eddf0b62c11bd454f0ff20
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-20 22:50:29 +0500

    Delete Question no 1 on DOM manipulation directory

--- Question no 1 on DOM manipulation/script.js
+++
@@ -1,6 +0,0 @@
-/* Question : Create a navbar and change the color of its first element to red.*/
-document.getElementsByTagName("nav")[0].firstElementChild.style.color="red"
-
-/* Create a table without tbody Now use "view page source" to check whether it has a tbody or not.
-
- The answer is no because view page source shows the raw source of it and tbody is autocorrected there. 
