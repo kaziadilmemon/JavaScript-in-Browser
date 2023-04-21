99840e1b26a78d51a69e0f4c740ade4fe6e90e2b
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-22 00:50:09 +0500

    Update HTML Insertion Methods.js

--- HTML Insertion Methods.js
+++ HTML Insertion Methods.js
@@ -5,7 +5,3 @@ let a = document.getElementsByTagName('div')[0]
 let div = document.createElement('div');
 div.innerHTML = '<h1>Hello World!</h1>';
  a.append(div); // append at the end of the node.
- a.prepend(div); // Insert at the beginning of the node.
- a.before(div);  // Insert before node.
- a.after(div);  // Insert after node.
- a.replaceWith(div); //replaces node with the given node.
