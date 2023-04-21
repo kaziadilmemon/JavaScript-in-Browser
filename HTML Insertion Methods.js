d5f42658cc001faaa9e0e30c1399ef3602ad99b8
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-22 00:49:53 +0500

    Create HTML Insertion Methods.js

---
+++ HTML Insertion Methods.js
@@ -0,0 +1,11 @@
+let a = document.getElementsByTagName('div')[0]
+
+// a.innerHTML = a.innerHTML + '<h1>Hello World!</h1>';
+
+let div = document.createElement('div');
+div.innerHTML = '<h1>Hello World!</h1>';
+ a.append(div); // append at the end of the node.
+ a.prepend(div); // Insert at the beginning of the node.
+ a.before(div);  // Insert before node.
+ a.after(div);  // Insert after node.
+ a.replaceWith(div); //replaces node with the given node.
