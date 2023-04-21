ce594bb8d85817985e6f6ab591db518480a5e69e
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-21 23:16:44 +0500

    Update HTML Attributes and their methods.js

--- HTML Attributes and their methods.js
+++ HTML Attributes and their methods.js
@@ -1,10 +1,21 @@
 let first = document.getElementById("first");
+/* 1 elem.getAttribute(name) : Method used to get the value of an attribute. */
+
 let a = first.getAttribute("class")
 // console.log(a)
+
+/* 2 elem.hasAttribute(name) : Method to check for existence of an attribute. */
+
 // console.log(first.hasAttribute("class"))
 // console.log(first.hasAttribute("style"))
+
+/* 3 elem.setAttribute(name,value) : Method used to set the value of an attribute. */
+
 // first.setAttribute("hidden", "true")
 // first.setAttribute("class", "true sachin")
+
+/* 4 elem.removeAttribute(name) :
+
 // first.removeAttribute("class")
 // console.log(first.attributes)
 console.log(first.dataset)
