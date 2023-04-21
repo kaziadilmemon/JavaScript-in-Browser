d5f1e6bc7ffff3a3c553ba1027855b0440a5a159
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-21 23:28:05 +0500

    Update HTML Attributes and their methods.js

--- HTML Attributes and their methods.js
+++ HTML Attributes and their methods.js
@@ -2,24 +2,22 @@ let first = document.getElementById("first");
 /* 1 elem.getAttribute(name) : Method used to get the value of an attribute. */
 
 let a = first.getAttribute("class")
-// console.log(a)
+console.log(a)
 
 /* 2 elem.hasAttribute(name) : Method to check for existence of an attribute. */
 
-// console.log(first.hasAttribute("class"))
-// console.log(first.hasAttribute("style"))
+console.log(first.hasAttribute("class"))
+console.log(first.hasAttribute("style"))
 
 /* 3 elem.setAttribute(name,value) : Method used to set the value of an attribute. */
 
-// first.setAttribute("hidden", "true")
-// first.setAttribute("class", "true sachin")
+first.setAttribute("hidden", "true")
+first.setAttribute("class", "true Adil")
 
 /* 4 elem.removeAttribute(name) : Method to remove the attribute from element */
 
-// first.removeAttribute("class")
+first.removeAttribute("class")
 
 /* 5 elem.attributes : Method to get the Collection of all attributes. */
 
-// console.log(first.attributes) 
-
-/* We can always 
+console.log(first.attributes) 
