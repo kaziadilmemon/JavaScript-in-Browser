c1af66ca6c6d6ee74ab27ac5e11733bf7b107666
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-22 22:02:46 +0500

    Update Handling Browser Events.js

--- Browser Events/Handling Browser Events.js
+++ Browser Events/Handling Browser Events.js
@@ -1,1 +1,28 @@
+/* When an event happens, the browser creates an event object, puts deatils into it.
+* event.type : event type.
+* event.currentTarget : element that handeled the event.
+* event.clientX/event.clientY : coordinates of the cursor. */
+
+let x = function(event) {
+  console.log(event.target)
+  console.log(event.type, event.clientX, event.clientY)
+  // alert("Hello World1!")
+}
+
+let y = function(e) {
+  console.log(e)
+  alert("Hello World2!")
+}
+
+btn.addEventListener('click', x)
+
+
+// btn.addEventListener('click', y)
+
+// let a = prompt("What is your favorite number?");
+
+
+// if (a == "2") {
+//   btn.removeEventListener('click', x)
+// }
 
