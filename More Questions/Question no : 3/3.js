7fe599144a53a59a7b994f4362385c07bd7a2f23
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-22 23:41:27 +0500

    Create 3.js

---
+++ More Questions/Question no : 3/3.js
@@ -0,0 +1,13 @@
+ /* Question : Write a JavaScript program to keep fetching contents of a website (every 5 seconds) */
+const fetchContent = async (url) => {
+  con = await fetch(url);
+  let a = await con.json()
+  return a;
+}
+
+setInterval(async function() {
+  let url = "https://jsonplaceholder.typicode.com/todos/1"
+  console.log(await fetchContent(url))
+}, 3000)
+
+
