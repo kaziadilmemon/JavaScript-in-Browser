6eb4766343b5a292d881a6c0f3628e92af8190c6
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-24 20:37:58 +0500

    Update Syncronous and Asyncronous.js

--- Callbacks/Syncronous and Asyncronous.js
+++ Callbacks/Syncronous and Asyncronous.js
@@ -1,13 +1,1 @@
-/* Asyncronous actions are the actions that we initiate now and finish later. eg:setTimeout */
 
-// Asyncronous Programming : 
-
-console.log("start")
-setTimeout(function() {
-  console.log("Hey I am good")
-},300)
-console.log("End")
-
-/* Syncronous actions are the actions that initiate and finish one-by-one */
-
-// Syncronous Programming : 
