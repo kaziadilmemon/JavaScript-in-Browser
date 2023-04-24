e8fb6f61ac2d7ab1a054402805cd19ec1e48eba5
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-24 22:45:07 +0500

    Update Introduction.js

--- Callbacks/Introduction.js
+++ Callbacks/Introduction.js
@@ -1,3 +1,1 @@
-/* A Callback Function is a Function based into another function as an argument, which is then invoked inside the Outer function to Complete an action. 
-                 ***Handling Error:
-We can handle callback errors by supplying error argument. */              
+
