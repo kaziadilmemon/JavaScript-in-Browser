0a62de926107da819d6d23a81000eef51f95a45f
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-26 12:30:42 +0500

    Update Accessing Children of an Element.js

--- Accessing Children of an Element.js
+++ Accessing Children of an Element.js
@@ -8,4 +8,4 @@ child nodes => Element that are direct children
 For example: head and body are children of <html>
 
   ChildNodes looks like an array but it not actually an array but a collective we can use Arrayfrom(collection) to convert it into an 
-  Array. (Array methods can't work)
+  Array. (Array methods can't work) */
