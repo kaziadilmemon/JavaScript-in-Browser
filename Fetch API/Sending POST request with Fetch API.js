cc087f87935b5fa610825b8ff48d98d65cab0aaa
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-05-01 21:49:36 +0500

    Update Sending POST request with Fetch API.js

--- Fetch API/Sending POST request with Fetch API.js
+++ Fetch API/Sending POST request with Fetch API.js
@@ -1,1 +1,33 @@
+// To make a post request,we need to use fetch options.
 
+const createTodo = async (todo) => {
+  let options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(todo),
+  }
+  let p = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+  let response = await p.json()
+  return response
+}
+
+const getTodo = async (id) => {
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+  let r = await response.json()
+  return r
+}
+
+const mainFunc = async () => {
+  let todo = {
+    title: 'Adil',
+    body: 'bhai',
+    userId: 100,
+  }
+  let todor = await createTodo(todo)
+  console.log(todor)
+  console.log(await getTodo(101))
+}
+
+mainFunc()
