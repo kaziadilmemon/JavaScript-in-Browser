71caf89061c9eabc48a0796b4d316d14e4888c9b
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-05-01 23:15:19 +0500

    Create script.js

---
+++ Cookies/script.js
@@ -0,0 +1,27 @@
+/* 
+Cookies are small Strings of data stored directly in the browser.In javascript,document.cookie provides access to Cookies.
+
+* The encodeURIComponent is a function that helps keep the valid formating.
+
+* Cookies have several options which can be provided after key=value
+
+* Note:
+
+  The name=value pair,after encodeURIComponent should not exceed 4KB.
+
+  Total no of cookies per domain is limited to around 20+(exact number is 
+  browser dependent 
+ 
+ */
+
+
+
+
+console.log(document.cookie)
+document.cookie = "name=harry1122334400"
+document.cookie = "name2=harry11223344002"
+document.cookie = "name=harry"
+let key = prompt("enter your key")
+let value = prompt("enter your value")
+document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+console.log(document.cookie)
